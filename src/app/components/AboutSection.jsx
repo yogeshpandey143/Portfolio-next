f7b1ed7d28f107236d622542ab7b6fabfa6cfd7f
Yogesh Pandey <bc21143@smsvaranasi.in>
2025-03-18 15:14:28 +0530

    build portfolio website

---
+++ src/app/components/AboutSection.jsx
@@ -0,0 +1,106 @@
+"use client";
+import React, { useTransition, useState } from "react";
+import Image from "next/image";
+import TabButton from "./TabButton";
+
+const TAB_DATA = [
+  {
+    title: "Skills",
+    id: "skills",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Java</li>
+        <li>Node.js</li>
+        <li>Express</li>
+        <li>SpringBoot</li>
+        <li>JavaScript</li>
+        <li>Next</li>
+        <li>SQL</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Education",
+    id: "education",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Master&apos;s Of Computer Application</li>
+        <li>Bachelor&apos;s Of Computer Application</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Certifications",
+    id: "certifications",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Artificial Intelligence (NASBA)</li>
+        <li>Google Professional Cloud Developer</li>
+      </ul>
+    ),
+  },
+];
+
+const AboutSection = () => {
+  const [tab, setTab] = useState("skills");
+  const [isPending, startTransition] = useTransition();
+
+  const handleTabChange = (id) => {
+    startTransition(() => {
+      setTab(id);
+    });
+  };
+
+  return (
+    <section className="text-white" id="about">
+      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+        <Image
+          src="/images/about-image.png"
+          width={500}
+          height={500}
+          alt="About section image"
+        />
+        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+          <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
+          <p className="text-base lg:text-lg">
+            Hi, I&apos;m Yogesh Pandey, a passionate Full-Stack Developer with
+            expertise in Java, JavaScript, React/Next.js, and Spring Boot. With
+            a strong foundation in backend and frontend development, I love
+            building scalable, high-performance web applications that enhance
+            user experience and operational efficiency. Currently, I&apos;m
+            pursuing my Master&apos;s of Computer Application (MCA) at Kamla
+            Nehru Institute of Technology, and I have hands-on experience as a
+            Software Engineer Intern at DTS TechIndia, where I optimized
+            e-commerce checkout flows, integrated Razorpay payment gateways, and
+            improved page performance by 90%.
+          </p>
+          <div className="flex flex-row justify-start mt-8">
+            <TabButton
+              selectTab={() => handleTabChange("skills")}
+              active={tab === "skills"}
+            >
+              Skills
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("education")}
+              active={tab === "education"}
+            >
+              Education
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("certifications")}
+              active={tab === "certifications"}
+            >
+              Certifications
+            </TabButton>
+          </div>
+          <div className="mt-8">
+            {TAB_DATA.find((t) => t.id === tab).content}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutSection;
